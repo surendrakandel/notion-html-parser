7fcf9a67b3fcd8af9595e853a718e0ee0d071203
surendrakandel <20468112+surendrakandel@users.noreply.github.com>
2023-09-10 12:04:01 -0700

    updates

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const getHtml = require("./build/index");
 
 
 (async () => {
-    let data = await getHtml.default("secret_kyymFPGNFemfZ46rQ5VfsVIf24JHOX6yWjM1J762PJ4", "15137e82c6ad4749acd75e17ce5cf14f");
+    let data = await getHtml.default({
+        apiKey: "secret_kyymFPGNFemfZ46rQ5VfsVIf24JHOX6yWjM1J762PJ4",
+        pageId: "15137e82c6ad4749acd75e17ce5cf14f",
+    });
     console.log(data)
   })();
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@
 import  *  as notionParser from "./notion-to-md";
 import  { Client }  from "@notionhq/client";
 import { marked }  from 'marked';
-
-async function getHTML(apiKey: string, pageId: string) {
-    if(!apiKey || !pageId || apiKey.length < 10 || pageId.length < 10) throw new Error("apiKey or pageId is not defined");
+export interface Config {
+    apiKey: string;
+    pageId: string;
+}
+async function getHTML(config: Config) {
+    if(!config.apiKey || !config.pageId || config.apiKey.length < 10 || config.pageId.length < 10) throw new Error("apiKey or pageId is not defined");
     let notionClient = new Client({
-        auth: apiKey,
+        auth: config.apiKey,
     })
     const n2m = new notionParser.NotionToMarkdown({ notionClient: notionClient, config:{
         parseChildPages: true,
     }});
     const { results } = await notionClient.blocks.children.list({
-        block_id: pageId,
+        block_id: config.pageId,
     });
     const x = await n2m.blocksToMarkdown(results);
     let markDownString = n2m.toMarkdownString(x);
